de20e72835a60e3069e56a967d9fe1af085aed5d
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2019-05-11 15:27:20 -0400

    add in some dummy weights and get a basic click going

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,42 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class SecondPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: 0
+    }
+  }
+
+  updateTotal = () => {
+    this.setState(
+      { total: this.state.total + (25 * 2) }
+    )
+  }
+
+  render() {
+    return (
+    <Layout>
+      <SEO title="Barbell Calculator" />
+      <h1>Calculate your barbells in Metric!</h1>
+      <div className="weights-container">
+        <div className="weight red" onClick={this.updateTotal}>25kg</div>
+        <div className="weight blue">20kg</div>
+        <div className="weight yellow">15kg</div>
+        <div className="weight green">10kg</div>
+        <div className="weight white">5kg</div>
+        <div className="weight black">2.5kg</div>
+        <div className="weight silver">1.25kg</div>
+        <div className="weight silver">.5kg</div>
+        <div className="weight silver">.5kg</div>
+      </div>
+      <h3>Weight: {this.state.total}</h3>
+      <h3>Weight: {this.state.total * 2.20462}</h3>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+    )
+  }
+}
 
 export default SecondPage
