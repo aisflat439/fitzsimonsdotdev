494dad89a28eed315058b6027a05885bcca875b8
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2019-05-16 20:22:14 -0400

    add a post because I'm a dummy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// const path = require('path')
 
-// You can delete this file if you're not using it
+// exports.createPagesFromPosts = ({ graphql, actions }) => {
+//     const { createPageFromPost } = actions
+//     return new Promise((resolve, reject) => {
+//         graphql(`{
+
+//         }`)
+//     })
+// }
