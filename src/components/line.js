813c33b540c5a86c52be0539d91707e0456022d1
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2019-05-22 09:40:25 -0400

    add in some base kesha text

---
+++ src/components/line.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react"
+
+
+const Lyric = ({ lyric }) => <span>{lyric}</span>
--- src/pages/kesha.js
+++ src/pages/kesha.js
@@ -1,17 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { lyrics } from "../data/kesha"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Kesha Ipsum" keywords={[`Lorem Ipsum Generator`, `kesha ipsum`, `ipsum`]} />
-    <h1>We are the crazy people</h1>
-    <Link to="/page-2/">Barbell Calculator</Link>
-    <Link to="/kesha/">Kesha Ipsum</Link>
-  </Layout>
+const buildLorem = (numLines, ipsumArray) => {
+
+    let ipsumText = ""
+    for (let i = 0; i < numLines; i++) {
+      ipsumText +=
+        "" + ipsumArray[Math.floor(Math.random() * ipsumArray.length)]
+    }
+
+    // console.log("numLines ", numLines);
+    // [...Array(4)].forEach(function() {
+    //   console.log("gomggmo")
+    // })
+  
+  // let lorem = ""
+
+  // [...Array(numLines)].forEach((item, index) => lorem += ipsumArray[Math.floor(Math.random() * ipsumArray.length)])
+
+  // return lorem
+  return ipsumText
+}
+
+const IndexPage = () => {
+  // console.log("eeeyy", lyrics)
+  const [numLines, setLines] = useState(7)
+  let ipsumText = buildLorem(numLines, lyrics)
+
+  return (
+    <Layout>
+      <SEO
+        title="Kesha Ipsum"
+        keywords={[`Lorem Ipsum Generator`, `kesha ipsum`, `ipsum`]}
+      />
+      <h1>We are the crazy people</h1>
+      <div>
+        <label>Lines of text per paragraph</label>
+        <input type="number" defaultValue={7} />
+        <label> Number of Paragraphs</label>
+        <input type="number" defaultValue={3} />
+        <button>Copy to Clipboard</button>
+        <button>Generate</button>
+        <div className="ipsum-content-container">
+          <Lyric lyric={ipsumText} />
+        </div>
+      </div>
+      <Link to="/page-2/">Barbell Calculator</Link>
+      <Link to="/kesha/">Kesha Ipsum</Link>
+    </Layout>
+  )
+}
+
+const Lyric = ({lyric}) => (
+  <span>{lyric}</span>
 )
 
 export default IndexPage
