8b93d8ba500446a4c40e186193a0b04ef832959a
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2019-12-17 18:56:11 -0700

    improvement: extract footer

---
+++ src/components/footer.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+
+import styled from "styled-components"
+import { colors } from "../theme"
+
+const StyledFooter = styled.footer`
+  background: ${colors.secondary}
+`
+
+const Footer = () => {
+    return (
+        <StyledFooter>
+        <section>
+            <h5>email</h5>
+            <p>sign up for updates</p>
+        </section>
+        <section>
+            <h5>links</h5>
+            <ul>
+            <li>
+                <Link to="/tips">Tips</Link>
+            </li>
+            <li>
+                <Link to="/site-reviews">Site Reviews</Link>
+            </li>
+            <li>
+                <Link to="/products">Products</Link>
+            </li>
+            <li>
+                <Link to="/thoughts">Thoughts</Link>
+            </li>
+            </ul>
+        </section>
+        <section>
+            <h5>social</h5>
+            <ul>
+            <li>twitter</li>
+            <li>instagram</li>
+            <li>linkedin</li>
+            </ul>
+        </section>
+        © {new Date().getFullYear()}
+    </StyledFooter>
+    )
+}
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,10 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql, Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => (
@@ -35,30 +36,7 @@ const Layout = ({ children }) => (
           }}
         >
           <main>{children}</main>
-          <footer>
-            <section>
-              <h5>email</h5>
-              <p>sign up for updates</p>
-            </section>
-            <section>
-              <h5>links</h5>
-              <ul>
-                <li><Link to="/tips">Tips</Link></li>
-                <li><Link to="/site-reviews">Site Reviews</Link></li>
-                <li><Link to="/products">Products</Link></li>
-                <li><Link to="/thoughts">Thoughts</Link></li>
-              </ul>
-            </section>
-            <section>
-              <h5>social</h5>
-              <ul>
-                <li>twitter</li>
-                <li>instagram</li>
-                <li>linkedin</li>
-              </ul>
-            </section>
-            © {new Date().getFullYear()}
-          </footer>
+          <Footer />
         </div>
       </>
     )}
