8528cf51c5d5f661f2c8e5249134ef98b340b986
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2019-12-28 08:04:01 -0700

    improvement: move footer to MUI, no styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,15 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import Typography from "@material-ui/core/Typography"
+import  {makeStyles}  from "@material-ui/core"
+
+const useStyles = makeStyles(({palette}) => ({
+    root: {
+        backgroundColor: palette.secondary.main,
+    }
+}))
 
 const Footer = () => {
     const { allDirectory } = useStaticQuery(graphql`
@@ -13,35 +23,46 @@ const Footer = () => {
             }
         }
     `)
+
+    const classes = useStyles()
+
+
     return (
-        <div data-testid="footer">
-        <section>
-            <h5>email</h5>
-            <p>sign up for updates</p>
-        </section>
-        <section>
-            <h5>link</h5>
-            <ul>
-                { allDirectory.edges.map(({node}) => {
-                    if (node.base === 'deprecated' || node.base === 'images'|| node.base === 'posts') return null
-                        return (
-                            <li key={node.id}>
-                        <Link  to={`/${node.base}`}>{`${node.base}`}</Link>
-                    </li>
-                        )
-                        })}
-            </ul>
-        </section>
-        <section>
-            <h5>social</h5>
-            <ul>
-            <li>twitter</li>
-            <li>instagram</li>
-            <li>linkedin</li>
-            </ul>
-        </section>
+        <Box component="footer" data-testid="footer" className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} md={3}>
+                    <Box component="section" >
+                    <Typography component="h5">link</Typography>
+                    <ul>
+                        { allDirectory.edges.map(({node}) => {
+                            if (node.base === 'deprecated' || node.base === 'images'|| node.base === 'posts') return null
+                            return (
+                                <li key={node.id}>
+                                <Link  to={`/${node.base}`}>{`${node.base}`}</Link>
+                            </li>
+                                )
+                            })}
+                    </ul>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Box component="section" >
+                        <Typography component="h5">email</Typography>
+                        <Typography>I haven't made this yet... but eventually</Typography>
+                        <Typography>sign up for updates</Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <Typography>social</Typography>
+                    <ul>
+                    <li>twitter</li>
+                    <li>instagram</li>
+                    <li>linkedin</li>
+                    </ul>
+                </Grid>
+            </Grid>
         © {new Date().getFullYear()}
-    </div>
+    </Box>
     )
 }
 
