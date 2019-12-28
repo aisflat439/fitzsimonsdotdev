a40fcea7f438f627997e529f8ba278a5b2a47567
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2019-12-28 08:24:48 -0700

    improvement: remove top layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
     twitterUsername: "@fitzsimons.dev",
   },
   plugins: [
-    'gatsby-plugin-top-layout',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
