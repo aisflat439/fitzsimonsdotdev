22d6031d8c1868f87037a68edfeb0b67ec3c4908
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2020-01-01 07:05:59 -0700

    improvement: add redux, groundwork for dark/light theme

--- src/theme.js
+++ src/theme.js
@@ -4,17 +4,21 @@ import { createMuiTheme } from '@material-ui/core/styles';
 // A custom theme for this app
 const theme = createMuiTheme({
     palette: {
+        type: 'dark',
         primary: {
-            main: '#bada55',
+            light: '#64d8cb',
+            main: '#26a69a',
+            dark: '#00766c',
+            contrastText: '#f5f5f5',
         },
         secondary: {
-            main: '#bada55',
+            light: '#718792',
+            main: '#455a64',
+            dark: '#1c313a',
+            contrastText: '#e0f7fa',
         },
         error: {
             main: red.A400,
-        },
-        background: {
-            default: '#fff',
         },
     },
 });
