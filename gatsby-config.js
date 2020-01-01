d30a2b83f309a2e78b4fb7add3df8a70dd021299
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2019-12-31 19:24:23 -0700

    improvement: add GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,26 @@ module.exports = {
 
     `gatsby-transformer-remark`,
     `gatsby-plugin-netlify`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: " GTM-PNSX53M",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
