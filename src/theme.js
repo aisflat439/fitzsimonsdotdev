687679488df49d1de44a14ce99a09fdf36531111
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2019-12-31 18:12:09 -0700

    improvement: validate theme works

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: '#556cd6',
+            main: '#bada55',
         },
         secondary: {
             main: '#bada55',
