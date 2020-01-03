ed5b02037c27681e35e6dba02b4022592bbe87bc
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2020-01-03 12:09:33 -0700

    improvement: fix footer warning

--- src/components/__tests__/footer.spec.jsx
+++ src/components/__tests__/footer.spec.jsx
@@ -1,37 +1,42 @@
-import React from "react"
+import React, { Children } from "react"
 import { render } from "@testing-library/react"
 import { useStaticQuery } from "gatsby"
 
 import Footer from '../footer'
-
+jest.mock('@material-ui/core/Link', () => ({ children }) => <div>{children}</div>)
 
 describe('<Footer/>', () => {
   beforeEach(() => {
     useStaticQuery.mockReturnValue({
       allDirectory: {
         edges: [
-          {node: {
-            base: "deprecated",
-            id: 123,
-          }},
-          {node: {
-            base: "anything else",
-            id: 1234,
-          }},
+          {
+            node: {
+              base: "deprecated",
+              id: 123,
+            }
+          },
+          {
+            node: {
+              base: "anything else",
+              id: 1234,
+            }
+          },
         ]
       },
     }
-  ) })
+    )
+  })
 
   it("renders", () => {
     const { getByTestId } = render(<Footer />)
-    
+
     expect(getByTestId("footer")).toBeInTheDocument()
   })
 
   it("displays only the correct folders", () => {
     const { queryByText } = render(<Footer />)
-    
+
     expect(queryByText("deprecated")).not.toBeInTheDocument()
     expect(queryByText("images")).not.toBeInTheDocument()
     expect(queryByText("posts")).not.toBeInTheDocument()
