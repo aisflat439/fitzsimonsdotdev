72ff084daa24dbfb3e01557c975b262c9042199f
Devin Fitzsimons <devinfitzsimons@pop-os.localdomain>
2020-01-05 11:23:12 -0700

    bug: try docs version of form to validate netlify forms

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,16 +4,24 @@ import Input from '@material-ui/core/Input';
 import TextField from '@material-ui/core/TextField';
 
 const Contact = () => (
-    <Box display="flex" component="form" name="newsletter" method="post" noValidate autoComplete="off" data-netlify="true" data-netlify-honeypot="bot-field">
-        <Input type="hidden" name="bot-field" />
-        <TextField
-            label="Email"
-            id="mui-theme-provider-standard-input"
-            variant="outlined"
-            required
-        />
-        <Input type="submit" value="Sign Up" />
-    </Box>
+    <form name="contact" method="POST" data-netlify="true">
+        <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
+    // <Box display="flex" component="form" name="newsletter" method="post" noValidate autoComplete="off" data-netlify="true" data-netlify-honeypot="bot-field">
+    //     <Input type="hidden" name="bot-field" />
+    //     <TextField
+    //         label="Email"
+    //         id="mui-theme-provider-standard-input"
+    //         variant="outlined"
+    //         required
+    //     />
+    //     <Input type="submit" value="Sign Up" />
+    // </Box>
 )
 
 export default Contact
