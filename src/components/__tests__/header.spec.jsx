d96a67dfa99d081563a06f47d75ac0dc0c725cb8
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-03-10 19:23:57 -0600

    fix: implment header testing

--- src/components/__tests__/header.spec.jsx
+++ src/components/__tests__/header.spec.jsx
@@ -1,9 +1,44 @@
 import React from "react"
-import renderer from "react-test-renderer"
+import { render, fireEvent } from "@testing-library/react"
+import { useDispatch } from 'react-redux';
+
 import Header from "../header"
+
+jest.mock('@material-ui/core/AppBar', () => ({ children }) => <div>{children}</div>)
+jest.mock('@material-ui/core/Toolbar', () => ({ children }) => <div>{children}</div>)
+jest.mock('@material-ui/core/Typography', () => ({ children }) => <div>{children}</div>)
+jest.mock('@material-ui/core/Slide', () => ({ children }) => <div>{children}</div>)
+jest.mock('@material-ui/core/Link', () => 'Link')
+
+jest.mock('react-redux', () => ({
+  useDispatch: jest.fn()
+}))
+
+const renderWith = (overrides) => {
+  const props = {
+    siteTitle: "OMG the Site!",
+    ...overrides
+  }
+
+  return render(<Header {...props} />)
+}
 
 describe("Header", () => {
   it("renders correctly", () => {
-    expect(1).toBe(1)
+    const { getByText } = renderWith({
+      siteTitle: 'Title'
+    })
+
+    expect(getByText(/Title/i)).toBeInTheDocument()
+  })
+
+  it("dispatches toggle theme mode on click", () => {
+    const mockDispatch = jest.fn()
+    useDispatch.mockReturnValue(mockDispatch)
+    const { getByTestId } = renderWith()
+
+    expect(mockDispatch).not.toHaveBeenCalled()
+    fireEvent.click(getByTestId('theme-toggle'))
+    expect(mockDispatch).toHaveBeenCalled()
   })
 })
--- src/components/header.js
+++ src/components/header.js
@@ -12,11 +12,8 @@ import { Brightness4 } from "@material-ui/icons"
 
 import { toggleThemeMode } from '../redux/userPreferencesSlice'
 
-const HideOnScroll = (props) => {
+export const HideOnScroll = (props) => {
   const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
   return (
@@ -60,7 +57,7 @@ const Header = ({ siteTitle }, props) => {
               {siteTitle}
             </Link>
           </Typography>
-          <IconButton onClick={onClick}>
+          <IconButton data-testid="theme-toggle" onClick={onClick}>
             <Brightness4 />
           </IconButton>
         </Toolbar>
