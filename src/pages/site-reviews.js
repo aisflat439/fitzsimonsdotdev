9e7a5363d7a3d9fd9e8b36b0295afd2db0a56116
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-03-14 05:40:37 -0600

    chore: handle gatsby warn about gql

--- src/pages/site-reviews.js
+++ src/pages/site-reviews.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { makeStyles } from '@material-ui/core/styles';
+import { graphql } from 'gatsby'
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
