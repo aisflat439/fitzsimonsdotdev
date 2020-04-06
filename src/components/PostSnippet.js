aebbf102e445261a3a794d72b61b7dccfe0c104c
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-04-06 08:39:06 -0600

    feat: inital styling of code snippets

--- src/components/PostSnippet.js
+++ src/components/PostSnippet.js
@@ -2,6 +2,7 @@ import React from "react"
 import Typography from "@material-ui/core/Typography"
 import Link from './Link'
 import SEO from './seo'
+import Markdown from "./Markdown"
 
 
 const PostSnippet = ({ title, timeToRead, content, slug }) => (
@@ -9,7 +10,9 @@ const PostSnippet = ({ title, timeToRead, content, slug }) => (
         <SEO canonical={`https://www.fitzsimons.dev${slug}`} />
         <Typography component="h1" variant="h5" display="inline">{title}</Typography>
         <Typography>~{timeToRead} read time</Typography>
-        <div dangerouslySetInnerHTML={{ __html: content }} />
+        <Markdown>
+            {content}
+        </Markdown>
     </>
 )
 
