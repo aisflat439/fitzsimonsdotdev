f089f7c842888eebab59032faaa24c9a22137f34
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-10-05 16:53:46 -0600

    feat: rip out header

--- src/components/__tests__/header.spec.jsx
+++ src/components/__tests__/header.spec.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { useDispatch } from 'react-redux';
+import { render } from '@testing-library/react';
 
 import { ThemeProvider } from 'styled-components';
 import theme from '../../theme';
 
 import Header from '../header';
-
-jest.mock('@material-ui/core/AppBar', () => ({ children }) => <div>{children}</div>);
-jest.mock('@material-ui/core/Toolbar', () => ({ children }) => <div>{children}</div>);
-jest.mock('@material-ui/core/Typography', () => ({ children }) => <div>{children}</div>);
-jest.mock('@material-ui/core/Hidden', () => ({ children }) => <div>{children}</div>);
-jest.mock('@material-ui/core/Slide', () => ({ children }) => <div>{children}</div>);
-jest.mock('@material-ui/core/Link', () => 'Link');
-
-jest.mock('react-redux', () => ({
-  useDispatch: jest.fn()
-}));
 
 const renderWith = (overrides) => {
   const props = {
@@ -38,15 +26,5 @@ describe('Header', () => {
     });
 
     expect(getAllByText(/Title/i)[0]).toBeInTheDocument();
-  });
-
-  it('dispatches toggle theme mode on click', () => {
-    const mockDispatch = jest.fn();
-    useDispatch.mockReturnValue(mockDispatch);
-    const { getByTestId } = renderWith();
-
-    expect(mockDispatch).not.toHaveBeenCalled();
-    fireEvent.click(getByTestId('theme-toggle'));
-    expect(mockDispatch).toHaveBeenCalled();
   });
 });
--- src/components/header.js
+++ src/components/header.js
@@ -1,82 +1,23 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'gatsby';
 import T from 'prop-types';
-import { makeStyles, useScrollTrigger } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Hidden from '@material-ui/core/Hidden';
-import Typography from '@material-ui/core/Typography';
-import Slide from '@material-ui/core/Slide';
-import { Brightness4 } from '@material-ui/icons';
 import styled from 'styled-components';
 
-import { toggleThemeMode } from '../redux/userPreferencesSlice';
 import HeaderLinks from './HeaderLinks';
 
-export const HideOnScroll = (props) => {
-  const { children, window } = props;
-  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+export const HideOnScroll = () => (<></>);
 
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-};
-
-const useStyles = makeStyles(({ palette }) => ({
-  root: {
-    background: `linear-gradient(to top, ${palette.primary.main} 10%, ${palette.primary.contrastText} 10%)`
-  },
-  title: {
-    flexGrow: 1
-  },
-  width: {
-    maxWidth: 1440,
-    width: '100%',
-    margin: '0 auto',
-    padding: '0 24px'
-  }
-}));
 
 const StyledHeader = styled.header`
   border-top: 5px solid #d1ad70;
   border-bottom: 5px solid #d1ad70;
-  background-color: ${({ theme }) => theme.palette.main};
+  background: linear-gradient(to right, ${({ theme }) => theme.palette.main.base} 50%, ${({ theme }) => theme.palette.main.saturated} );
 `;
 
-const Header = ({ siteTitle }, props) => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  const onClick = () =>
-    dispatch(toggleThemeMode());
-
-
-  return (
-    <>
-      <StyledHeader>
-        <HeaderLinks siteTitle={siteTitle} />
-      </StyledHeader>
-      <Hidden xsUp>
-        <HideOnScroll {...props}>
-          <AppBar className={classes.root}>
-            <Toolbar className={classes.width}>
-              <Typography className={classes.title}>
-                <HeaderLinks siteTitle={siteTitle} />
-              </Typography>
-              <IconButton data-testid="theme-toggle" onClick={onClick}>
-                <Brightness4 />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-        </HideOnScroll>
-      </Hidden>
-    </>
-  );
-};
+const Header = ({ siteTitle }) => (
+  <StyledHeader>
+    <HeaderLinks siteTitle={siteTitle} />
+  </StyledHeader>
+);
 
 Header.propTypes = {
   siteTitle: T.string,
