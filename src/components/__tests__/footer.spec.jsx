4e23adcc960b3edc0e68d685e935dbb7a661f030
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-10-11 16:09:48 -0600

    feat: inital testing of new footer

--- src/components/__tests__/footer.spec.jsx
+++ src/components/__tests__/footer.spec.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import { useStaticQuery } from 'gatsby';
 
 import { ThemeProvider } from 'styled-components';
@@ -65,5 +66,89 @@ describe('<Footer/>', () => {
 
     expect(queryByText('query')).toBeInTheDocument();
     expect(queryByText('response')).toBeInTheDocument();
+  });
+
+  it('renders 3 links', () => {
+    useStaticQuery.mockReturnValueOnce({
+      allDirectory: {
+        edges: [
+          {
+            node: {
+              base: 'query',
+              id: 123,
+            }
+          },
+        ]
+      },
+      site: {
+        siteMetadata: {
+          identityData: [
+            {
+              siteName: 'One',
+              siteLink: 'One-link',
+            },
+            {
+              siteName: 'Two',
+              siteLink: 'Two-link',
+            },
+            {
+              siteName: 'Three',
+              siteLink: 'Three-link',
+            },
+            {
+              siteName: 'Four',
+              siteLink: 'Four-link',
+            },
+          ]
+        }
+      }
+    });
+
+    const { queryByText, getByText } = setup();
+
+    expect(getByText(/one/i)).toBeInTheDocument();
+    expect(queryByText(/Four/i)).not.toBeInTheDocument();
+  });
+
+  it('shows and hides all links', () => {
+    useStaticQuery.mockReturnValueOnce({
+      allDirectory: {
+        edges: [
+          {
+            node: {
+              base: 'query',
+              id: 123,
+            }
+          },
+        ]
+      },
+      site: {
+        siteMetadata: {
+          identityData: [
+            {
+              siteName: 'One',
+              siteLink: 'One-link',
+            },
+            {
+              siteName: 'Two',
+              siteLink: 'Two-link',
+            },
+            {
+              siteName: 'Three',
+              siteLink: 'Three-link',
+            },
+            {
+              siteName: 'Four',
+              siteLink: 'Four-link',
+            },
+          ]
+        }
+      }
+    });
+
+    const { queryByText, getByText, getByRole } = setup();
+
+    expect(queryByText(/Four/i)).not.toBeInTheDocument();
+    // userEvent.click(getByRole('button'));
   });
 });
