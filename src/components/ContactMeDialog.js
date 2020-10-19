4212e87e327193a878a0148c60c8d4d67e3b9cce
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-10-19 12:02:38 -0600

    feat: add form testing

--- src/components/ContactMeDialog.js
+++ src/components/ContactMeDialog.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -9,21 +9,19 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import { useDispatch } from 'react-redux';
 
-import { requestReview } from '../redux/actions/gtmActions'
+import { requestReview } from '../redux/actions/gtmActions';
 import Contact from './Contact';
 import NewsletterSignup from './NewsletterSignup';
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
+const Transition = React.forwardRef((props, ref) => <Slide direction="up" ref={ref} {...props} />);
 
 const ContactMeDialog = ({ title, form }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [open, setOpen] = React.useState(false);
-  const isContactForm = form === "contact"
+  const isContactForm = form === 'contact';
 
   const handleOpen = (e) => {
-    dispatch(requestReview(e.target.textContent))
+    dispatch(requestReview(e.target.textContent));
     setOpen(!open);
   };
 
@@ -42,11 +40,9 @@ const ContactMeDialog = ({ title, form }) => {
         aria-labelledby="alert-dialog-slide-title"
         aria-describedby="alert-dialog-slide-description"
       >
-        {isContactForm ?
-          (<DialogTitle id="alert-dialog-slide-title">Request a site review</DialogTitle>)
-          :
-          (<DialogTitle id="alert-dialog-slide-title" data-testid="newsletter-title">Sign up for updates</DialogTitle>)
-        }
+        {isContactForm
+          ? (<DialogTitle id="alert-dialog-slide-title">Request a site review</DialogTitle>)
+          : (<DialogTitle id="alert-dialog-slide-title" data-testid="newsletter-title">Sign up for updates</DialogTitle>)}
 
         <DialogContent>
           {isContactForm ? (
@@ -54,13 +50,13 @@ const ContactMeDialog = ({ title, form }) => {
               <DialogContentText id="alert-dialog-slide-description">
                 Every now and again I look talking through what I do and don't like about a website. If you're so inclined, I'm happy to review your site! It's just one persons opinion but I'll do my best to provide actionable feedback and suggestions. You're under no obligation to do anything with the review. It's just a random person on the internet saying what they like and dislike about a site on YouTube.
               </DialogContentText>
-              <Contact />
+              <Contact handleClose={handleClose} />
             </>
           ) : (
               <>
                 <DialogContentText id="alert-dialog-slide-description">
                   Sign up for updates!
-                </DialogContentText>
+              </DialogContentText>
                 <NewsletterSignup />
               </>
             )}
@@ -72,7 +68,7 @@ const ContactMeDialog = ({ title, form }) => {
         </DialogActions>
       </Dialog>
     </>
-  )
-}
+  );
+};
 
-export default ContactMeDialog
+export default ContactMeDialog;
