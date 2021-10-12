58d5952d33d285634d8928c878400c841e7dcf29
Devin <fitzsimonsdevin@gmail.com>
2021-10-12 14:34:53 -0600

    prettier a little

--- src/components/HomePageCardContent.js
+++ src/components/HomePageCardContent.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const HomePageCardContent = ({ avatar, title, subheader, content }) => (
   <>
     <div>
       <div className="avatar">
-        {avatar.split('').map((letter) => (
-          <span>{letter}</span>
-        ))}
+        {avatar.split('').map((letter, index) => {
+          const square = !!((Math.floor(Math.random() * 10) + 1) % 2);
+          const tilt = !!((Math.floor(Math.random() * 10) + 1) % 2);
+          const shadeOne = !!((Math.floor(Math.random() * 10) + 1) % 2);
+          const shadeTwo = !!((Math.floor(Math.random() * 10) + 1) % 2);
+          const borderOne = !!((Math.floor(Math.random() * 10) + 1) % 2);
+          const borderTwo = !!((Math.floor(Math.random() * 10) + 1) % 2);
+          return (
+            <span
+              // eslint-disable-next-line react/no-array-index-key
+              key={`${letter}-${index}`}
+              className={classNames({
+                square,
+                tilt,
+                shadeOne,
+                shadeTwo,
+                borderOne,
+                borderTwo,
+              })}
+            >
+              <span>{letter}</span>
+            </span>
+          );
+        })}
       </div>
       <h2>{title}</h2>
-      <p>{subheader}</p>
+      <p className="subheader">{subheader}</p>
     </div>
     <div>
-      <p>{content}</p>
+      <p className="content">{content}</p>
     </div>
   </>
 );
