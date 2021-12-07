92d6b90d4cba494ab459a8ebd98be4adb27efd03
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2021-12-07 13:39:07 -0700

    Erm (#67)
    * post about state machines
    
    * fix: hahah you build buggy stuff

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,43 +1,48 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import T from 'prop-types';
 
-import { Link, graphql } from "gatsby"
-import Layout from "../components/layout"
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/layout';
 
 const Tags = ({ pageContext, data }) => {
-  const { tag } = pageContext
-  const { edges, totalCount } = data.allMarkdownRemark
+  const { tag } = pageContext;
+  const { edges, group } = data.allMdx;
+  const { totalCount } = group.find((g) => g.tag === tag);
   const tagHeader = `${totalCount} post${
-    totalCount === 1 ? "" : "s"
-    } tagged with "${tag}"`
+    totalCount === 1 ? '' : 's'
+  } tagged with "${tag}"`;
 
   return (
     <Layout>
       <h1>{tagHeader}</h1>
       <ul>
         {edges.map(({ node }) => {
-          const { slug } = node.fields
-          const { title } = node.frontmatter
+          const { slug } = node.fields;
+          const { title } = node.frontmatter;
           return (
             <li key={slug}>
               <Link to={slug}>{title}</Link>
             </li>
-          )
+          );
         })}
       </ul>
       <Link to="/categories">All categories</Link>
     </Layout>
-  )
-}
-export default Tags
+  );
+};
+export default Tags;
+
 export const pageQuery = graphql`
   query($tag: String) {
-    allMarkdownRemark(
+    allMdx(
       limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { hashtags: { in: [$tag] } } }
     ) {
-      totalCount
+      group(field: frontmatter___hashtags) {
+        tag: fieldValue
+        totalCount
+      }
       edges {
         node {
           fields {
@@ -50,4 +55,9 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
+
+Tags.propTypes = {
+  pageContext: T.shape().isRequired,
+  data: T.shape().isRequired,
+};
