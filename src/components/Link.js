a02b030712f35ab81f49bbed081ffcddfc468fe1
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2022-08-24 17:48:02 -0600

    Astro (#69)
    * yolo update to astro
    
    * chore: remove unused import

--- src/components/Link.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link as GatsbyLink } from 'gatsby';
-
-const Link = styled((props) => <GatsbyLink {...props} />)`
-  text-decoration: none;
-`;
-
-export default Link;
--- src/utils/stringUtils/avatarLengthHandler.js
+++
@@ -1,8 +0,0 @@
-const avatarLengthHandler = (str) => {
-  if (str.length > 4) {
-    return [...str][0].toUpperCase()
-  }
-  return str.slice(0, 1).toUpperCase().concat(str.slice(1))
-}
-
-export default avatarLengthHandler
