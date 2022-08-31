6a81b048586d5b828ca5a79a5ca3ad7b363cda60
Devin <fitzsimonsdevin@gmail.com>
2022-08-31 16:04:37 -0600

    feat: resolve issues

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), tailwind(), image()],
+});
